f8fac8a304254d35134d5cfc6917f1ac455278aa
devangelista2 <devangelista2@users.noreply.github.com>
2022-11-12 17:13:15 +0000

    jekyll build from Action 4b44c94c2a4015f741c4186d3714fad31cbf6901

---
+++ assets/poly_regression.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Generate the test problem
+
+# Set dimensionality
+N = 100
+
+# y = wx + noise in the interval [a, b]
+a = -1
+b = 1
+
+w1 = 0
+w2 = 2
+w3 = 3
+w4 = 1
+noise_level = 0.1
+
+# Define X
+X = np.linspace(a, b, N)
+
+# Define Y
+Y = w4 * X**3 + w3 * X**2 + w2 * X + w1 + noise_level * (b - a) * np.random.normal(0, 1, X.shape)
+
+# Define the real line 
+Y_real = w4 * X**3 + w3 * X**2 + w2 * X + w1
+
+# Plot
+plt.plot(X, Y, 'o')
+plt.plot(X, Y_real, 'r')
+plt.grid()
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('An Example of Regression')
+plt.show()
